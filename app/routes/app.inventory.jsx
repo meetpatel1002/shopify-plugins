13a5ca05177d8a2d56a5c489746d155e36f41e76
meetpatel1002 <meetspatel92@gmail.com>
2024-06-07 18:42:07 +0530

    fixes inventory

--- app/routes/app.inventory.jsx
+++ app/routes/app.inventory.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-import { authenticate } from '../shopify.server';
-import { useLoaderData } from '@remix-run/react';
+import React from "react";
+import { authenticate } from "../shopify.server";
+import { useLoaderData } from "@remix-run/react";
 
-export async function loader({request}) {
-    const { admin } = await authenticate.admin(request);
+export async function loader({ request }) {
+  const { admin, session } = await authenticate.admin(request);
 
-    const response = await admin.graphql(
-      `#graphql
-      query {
-        inventoryLevel(id: "gid://shopify/InventoryLevel/523463154?inventory_item_id=30322695") {
-          id
-          quantities(names: ["available", "incoming", "committed", "damaged", "on_hand", "quality_control", "reserved", "safety_stock"]) {
-            name
-            quantity
-          }
-          item {
-            id
-            sku
-          }
-          location {
-            id
-            name
-          }
-        }
-      }`,
-    );
+  try {
+    const response = await admin.rest.resources.InventoryLevel.all({
+      session: session,
+      location_ids: "71945126065",
+    });
 
-    const data = await response.json();
+    if (response) {
+      console.log("hit");
+
+      const data = response.data;
+      console.log(data, "data");
+
+      return json({
+        inventory: data,
+      });
 
 
-    const inventoryItem = data.data.inventoryLevel;
-    return inventoryItem;
+    }
+
+
+    return null;
+
+  } catch (error) {
+    console.log(error, "error");
+  }
 }
 
 const Inventory = () => {
-    const getInventory = useLoaderData();
-    console.log(getInventory );
-  return (
-    <div> 
-        this is inventoryItems
-    </div>
-  )
-}
+  const data = useLoaderData();
+  console.log(data, "data");
+  return <div>this is inventoryItems</div>;
+};
 
-export default Inventory
+export default Inventory;
+
+
